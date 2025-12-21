f744a743899383417c9a8bf38ddd5425c616c72b
Mziomukulu Mike Kumalo <81001018+mikemuzi@users.noreply.github.com>
2025-12-21 17:46:19 -0600

    update 3

--- send_email.php
+++ send_email.php
@@ -3,25 +3,41 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
+    $phone = $_POST["phone"];
     $message = $_POST["message"];
 
     // Set the recipient email address
     $to = "mikemuzi73@live.com";
 
     // Set the subject of the email
-    $subject = "New Contact Form Submission";
+    $subject = "New Contact Form from  Submission";
+
+ // Email body
+    $body = "
+    - Customer Application Form -
+
+    Full Name: $name
+    Email: $email
+    Phone: $phone
+    
+    Customer Message:
+    $message
+
+    -------------------------------
+    Sent on: " . date("Y-m-d H:i:s") . "
+    ";
+
+    
 
     // Build the email content
     $email_content = "Name: $name\n";
     $email_content .= "Email: $email\n\n";
+    $email_content .= "Phone: $phone\n\n";
     $email_content .= "Message:\n$message";
 
-    // Send the email
-    mail($to, $subject, $email_content);
-
-    // Redirect to a thank-you page or display a success message
-    header("Location: thank_you.html");
-    exit;
+// Send email
+    $sent = mail($to, $subject, $body, $headers);
+    echo $sent ? "SUCCESS" : "ERROR";
 }
 
 ?>
